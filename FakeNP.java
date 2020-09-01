8d1ed920127c25c084713968aef6bd520736ac90
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2020-09-01 22:39:21 +0530

    FUN

---
+++ FakeNP.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class FakeNP {
+
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		
+		int l = in.nextInt();
+		int r = in.nextInt();
+		if(l==r)
+			System.out.println(l);
+		else
+		System.out.println(2);
+		
+		
+	}
+	
+}
