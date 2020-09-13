f0e13b8d78ddae3a200be2d2ccc64209d062ef84
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2020-09-13 16:21:49 +0530

    FUN

---
+++ DefiniteGame.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DefiniteGame {
+
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		
+		long v= in.nextLong();
+		if(v>2)
+		System.out.println(1);
+		else
+			System.out.println(v);
+	}
+	
+}
