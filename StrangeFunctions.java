3bbd76df40c56c2e284252f35654c2e06f99771a
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2020-11-30 21:53:29 +0530

    FUN

---
+++ StrangeFunctions.java
@@ -0,0 +1,73 @@
+ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import java.util.Scanner;
+import java.util.*;
+
+public class StrangeFunctions
+{
+	static class FastReader
+	{
+		BufferedReader br;
+		StringTokenizer st;
+		public FastReader(){
+			br = new BufferedReader(new
+			InputStreamReader(System.in));
+		}
+		String next(){
+			while (st == null || !st.hasMoreElements()){
+				try{
+					st = new StringTokenizer(br.readLine());
+				}
+				catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt(){
+			return Integer.parseInt(next());
+		}
+		long nextLong(){
+			return Long.parseLong(next());
+		}
+		double nextDouble(){
+			return Double.parseDouble(next());
+		}
+		String nextLine()
+		{
+			String str = "";
+			try{
+				str = br.readLine();
+			}
+			catch (IOException e){
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+	
+	public static void main(String[] args){
+		FastReader in=new FastReader();
+
+		int t = in.nextInt();
+		while(t>0) {
+			t--;
+			String s = in.nextLine();
+			System.out.println(s.length());
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+
+		
+        
+	}
+	
+}
