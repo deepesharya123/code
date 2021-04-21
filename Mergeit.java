afd501cef601470f283a737afed6fa7bfe7b2ba2
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2021-04-21 19:24:10 +0530

    FUN

---
+++ Mergeit.java
@@ -0,0 +1,157 @@
+//Merge it!
+
+	import java.io.BufferedReader;
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	//import java.util.Scanner;
+	import java.util.*;
+	
+	public  class t
+	{
+		static class FastReader
+		{
+			BufferedReader br;
+			StringTokenizer st;
+			public FastReader(){
+				br = new BufferedReader(new
+				InputStreamReader(System.in));
+			}
+			String next(){
+				while (st == null || !st.hasMoreElements()){
+					try{
+						st = new StringTokenizer(br.readLine());
+					}
+					catch (IOException e){
+						e.printStackTrace();
+					}
+				}
+				return st.nextToken();
+			}
+			int nextInt(){
+				return Integer.parseInt(next());
+			}
+			long nextLong(){
+				return Long.parseLong(next());
+			}
+			double nextDouble(){
+				return Double.parseDouble(next());
+			}
+			String nextLine()
+			{
+				String str = "";
+				try{
+					str = br.readLine();
+				}
+				catch (IOException e){
+					e.printStackTrace();
+				}
+				return str;
+			}
+		}
+		
+		public static void mul(int a,int b,int re) {
+			if(b>0) {
+				re+=a;
+			b--;
+			if(b==0)
+				System.out.println(re);
+			mul(a,b,re);
+		}
+	}
+		
+		public void merge(int[] a,int l,int m,int u) {
+			int n1 = m-l+1;
+			int n2 = u-m;
+			
+			int[] left = new int[n1];
+			int []right = new int[n2];
+			
+			int i,j,k;
+			for(i=0;i<n1;i++)
+				left[i]=a[i+l];
+			for(i=0;i<n2;i++)
+				right[i]=a[i+m+1];
+			
+			i=0;
+			j=0;
+			k=l;
+			while(i<n1&&j<n2) {
+				if(left[i]<right[j]) {
+					a[k]=left[i];
+					i++;
+				}else {
+					a[k]=right[j];
+					j++;
+				}
+				k++;
+			}
+			
+			while(i<n1) {
+				a[k]=left[i];
+				i++;
+				k++;
+			}
+			
+			while(j<n2) {
+				a[k]=right[j];
+				k++;
+				j++;
+			}
+			
+			
+			
+			
+		}
+		
+		public void sort(int[] a,int l,int u) {
+			if(l<u) {
+				int m = l+u-1;
+				m/=2;
+				
+				sort(a,l,m);
+				sort(a,m+1,u);
+				
+				merge(a,l,m,u);
+			}
+		}
+		
+	public static void main(String[] args){
+		FastReader in=new FastReader();
+		
+		int t = in.nextInt();
+		while(t>0) {
+			t--;
+			
+			int n = in.nextInt();
+			int i;
+			int[] rem = new int[3];
+			int[] ar=new int[n];
+			int con = 0;
+			for(i=0;i<n;i++) {
+				ar[i]=in.nextInt();
+				rem[ar[i]%3]++;
+			}
+			con+=rem[0];
+			int ad =Math.min(rem[1], rem[2]);
+			con+=ad;
+			int ss = 0;
+			if(ad==rem[1]) {
+				int cat = rem[2]-ad;
+				cat/=3;
+				ss+=cat;
+			}if(ad==rem[2]){
+				int cat = rem[1]-ad;
+				cat/=3;
+				ss+=cat;
+			}
+			con+=ss;
+			
+			System.out.println(con);
+			
+			
+			
+		}
+		
+		
+	}
+}
