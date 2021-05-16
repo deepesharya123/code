b0bf5000835c9c89b417dd774f3b55ddca084774
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2021-05-16 13:16:35 +0530

    FUN

---
+++ PartialReplacement.java
@@ -0,0 +1,166 @@
+//PartialReplacement
+
+	import java.io.BufferedReader;
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	//import java.util.Scanner;
+	import java.util.*;
+	
+	public  class t
+	{
+		static class FastReader
+		{
+			BufferedReader br;
+			StringTokenizer st;
+			public FastReader(){
+				br = new BufferedReader(new
+				InputStreamReader(System.in));
+			}
+			String next(){
+				while (st == null || !st.hasMoreElements()){
+					try{
+						st = new StringTokenizer(br.readLine());
+					}
+					catch (IOException e){
+						e.printStackTrace();
+					}
+				}
+				return st.nextToken();
+			}
+			int nextInt(){
+				return Integer.parseInt(next());
+			}
+			long nextLong(){
+				return Long.parseLong(next());
+			}
+			double nextDouble(){
+				return Double.parseDouble(next());
+			}
+			String nextLine()
+			{
+				String str = "";
+				try{
+					str = br.readLine();
+				}
+				catch (IOException e){
+					e.printStackTrace();
+				}
+				return str;
+			}
+		}
+		
+		public static void mul(int a,int b,int re) {
+			if(b>0) {
+				re+=a;
+			b--;
+			if(b==0)
+				System.out.println(re);
+			mul(a,b,re);
+		}
+	}
+	
+	public static void insertHeap(int[] ar,int n) {
+		
+		for(int i=1;i<n;i++) {
+			int pa= i-1;
+			pa/=2;
+			int ari = ar[i];
+			
+			if(ar[pa]<ar[i]) {
+				int temp = ar[pa];
+				ar[pa]=ar[i];
+				ar[i]=temp;
+			}
+			
+//			while(pa>=0&&ar[pa]<ar[i]) {
+//				ar[pa]=ari;
+//				i=pa;
+//				pa--;
+//				pa/=2;
+//			}
+			
+//			ar[i]=
+		}
+		
+		
+		
+	}
+		
+	
+	public static void print_arr(int[] ar,int n) {
+		int i;
+		for(i=0;i<n;i++)
+			System.out.print(ar[i]+" ");
+	}
+	public static void main(String[] args){
+		FastReader in=new FastReader();
+		
+		int t = in.nextInt();
+
+		while(t>0 ) {
+			t--;
+
+			int n = in.nextInt();
+			int k = in.nextInt();
+			String s = in.nextLine();
+			char[] c = s.toCharArray();
+			int i;
+			int start = -1,last=-1;
+			Vector<Integer> v = new Vector<Integer>();
+			
+			for(i=0;i<n;i++) {
+				if(c[i]=='*') {
+					if(start==-1)
+						start = i;
+					
+					last = i;
+					v.add(i);
+				}
+			}
+			int con = 0;
+			if(start>=0) {
+			c[start] = 'x';
+			c[last] ='x';
+			if(start==last )
+				con=1;
+			else
+				con=2;
+			
+			int tot =0;
+			int pre = v.get(0);
+// 			System.out.println(con+" ");
+			for(i=0;i<v.size();i++) {
+				// if(v.get(i)-pre>k&&v.size()>2) {
+				// 	con++;
+				// 	pre = v.get(i);
+				// }
+				// System.out.println("HEre "+i  );
+				int j = i+1;
+				while(j<v.size()&&v.get(j)-v.get(i)<=k){
+				    j++;
+				}
+				if(j>i&&j<v.size()&&v.get(j)-v.get(i)>0){
+//				    System.out.println("con "+con+" "+j);
+				    con++;
+				    i=j-2;
+				}
+			    	
+				
+			}
+			}
+			
+			System.out.println(con );
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+		
+}
