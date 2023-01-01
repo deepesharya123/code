fa62eb0635cc45830d61ab3cc066b381a5837743
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2023-01-01 12:35:23 +0530

    Time: 6 ms (5.97%), Space: 40.6 MB (66.98%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         boolean ans =true;
-        HashMap<String,String> map = new HashMap<>();
-        HashMap<String,String> map2 = new HashMap<>();
+        HashMap<Character,String> map = new HashMap<>();
+        
         String ss = "";
         int words =0;
         int j = 0;
@@ -15,20 +15,14 @@ class Solution {
             j++;
             if(j<=s.length())
             words++;
-            String comp =(String) map.getOrDefault(""+c,"");
+            
+            String comp =(String) map.getOrDefault(c,"");
+           
+            if( (map.containsValue(ss) && !map.containsKey(c)) || ( comp.length()>0&&!comp.equals(ss)))
+                    return false;
             if(comp.length()==0)
-                map.put(""+c,ss);
-            else{
-                if(!comp.equals(ss))
-                    return false;
-            }
-            String charComp = map2.getOrDefault(ss,"");
-            if(charComp.length()==0){
-                map2.put(ss,""+c);
-            }else{
-                if(!charComp.equals(""+c))
-                    return false;
-            }
+                map.put(c,ss); 
+            
             
             ss="";
         }
@@ -38,7 +32,7 @@ class Solution {
                 j++;
             }
                words++;
-        // System.out.println(s+" "+words);
+        
 
         if(words!=pattern.length())  return false;
         return true;
