8ec7f0e6e86a0e03a4e467916951b4303c081035
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2023-01-03 12:21:17 +0530

    Time: 11 ms (64.72%), Space: 47.8 MB (37.08%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -1,19 +1,15 @@
 class Solution {
     public int minDeletionSize(String[] strs) {
-        String[] ss = Arrays.copyOf(strs,strs.length);
         int ans = 0;
         for( int i =0;i<strs[0].length();i++){
-            final int ii  = i;
-            PriorityQueue<Character> pq = new PriorityQueue<>((s1, s2)-> (s1-s2));
-            for(int j =0;j<strs.length;j++){
-                pq.add(strs[j].charAt(i));
-            }
-            for(int j = 0;j<strs.length;j++){
-                if(ss[j].charAt(i)!=(pq.remove())){
+           char c = strs[0].charAt(i);
+            for(int j =1;j<strs.length;j++){
+                if(strs[j].charAt(i)<c){
                     ans++;
                     break;
-                }
-            }
+                }else
+                    c = strs[j].charAt(i);
+            }    
         }
         return ans;
     }
