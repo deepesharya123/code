9b37f2fa9de7289977289285124c874e8389ce05
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2023-01-03 18:55:44 +0530

    Time: 15 ms (5.86%), Space: 42.3 MB (60.32%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,48 +1,55 @@
 class Solution {
-    static int ans = 0;
-    
-    
-    public int orangesRotting(int[][] grid) {
-        ans = 0;
-        int i =0,j=0;
-        Queue<Pair<Integer,Pair<Integer,Integer>>> q = new LinkedList<>();
-        for(i=0;i<grid.length;i++)
-            for(j=0;j<grid[0].length;j++){
-                if(grid[i][j]==2){
-                    q.add(new Pair(i, new Pair(j,0)));            
-                }
+    static int time = 0;
+
+    public static void bfs(int[][] cg,
+    Queue<Pair<Pair<Integer,Integer>,Integer>> q,int[] dr,int[] dc){
+        Pair p = q.poll();
+        Pair pp =(Pair) p.getKey();
+        int r = (int) pp.getKey(); 
+        int c= (int) pp.getValue(); 
+        int t = (int) p.getValue();
+         int n = cg.length,m = cg[0].length;
+        int i = 0;
+        for(i=0;i<4;i++){
+            int nr = r+ dr[i];
+            int nc = c+dc[i];
+            if(nr<n && nr>=0 && nc<m && nc>=0 && cg[nr][nc]==1){
+                cg[nr][nc]=2;
+                time = Math.max(time,t+1);
+                System.out.println("TIme"+time);
+                q.add(new Pair(new Pair(nr,nc),t+1));   
             }
-        
-        int[] dr = {-1,0,1,0};
-        int[] dc = {0,1,0,-1};
-        int n = grid.length, m = grid[0].length;
-        while(!q.isEmpty()){
-            int r = q.peek().getKey();
-            int c = q.peek().getValue().getKey();
-            int t = q.peek().getValue().getValue();
-            
-            q.remove();
-            
-            ans = Math.max(t,ans);
-            for(i = 0;i<4;i++){
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1){
-                    q.add(new Pair(nr, new Pair(nc, t+1)));
-                    grid[nr][nc]=2;
-                }
-            }
-            
+
         }
-        
-        for(i=0;i<grid.length;i++)
-            for(j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1){
-                    ans=-1;
-                    break;
+
+        return ;
+    }
+
+    public int orangesRotting(int[][] grid) {
+        Queue<Pair<Pair<Integer,Integer>,Integer>> q = new LinkedList();
+        int n = grid.length,m = grid[0].length;
+        int[][] cg = new int[n][m];
+        int  i=0,j=0;
+        time = 0;
+        int[] dr = {1,0,-1,0};
+        int[] dc = {0,1,0,-1};
+        for(i=0;i<n;i++){
+            for(j=0;j<m;j++){
+                cg[i][j]=grid[i][j];
+                if(grid[i][j]==2){
+                    q.add(new Pair(new Pair(i,j),0));         
                 }
             }
+        }
+        while(q.size()>0)
+            bfs(cg,q,dr,dc);
         
-        return ans;
+        for(i=0;i<n;i++){
+            for(j=0;j<m;j++){
+                if(cg[i][j]==1)
+                    time = -1;
+            }
+        }
+        return time;
     }
 }
