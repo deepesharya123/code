480113df02530bf2edce1731e97f9bd815b4484f
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2023-01-08 16:53:34 +0530

    Time: 25 ms (83.64%), Space: 42.4 MB (74.23%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,27 +1,21 @@
 class Solution {
     public int maxPoints(int[][] points) {
-        int ans = 0;
-        
-        for(int i =0;i<points.length;i++){
+        int ans = 1, n = points.length;
+        for(int i = 0;i<n;i++){
             int x1 = points[i][0], y1 = points[i][1];
-            for(int j =0;j< points.length;j++){
+            int con = 0;
+            HashMap<Double,Integer> map = new HashMap<>();
+            for(int j = 0;j<n;j++){
                 if(i==j)
                     continue;
                 int x2 = points[j][0], y2 = points[j][1];
-                int con = 2;
-                for(int k = 0;k<points.length;k++){
-                    if(k==i || k==j )
-                        continue;
-                    int x = points[k][0], y= points[k][1];
-                    if(((y-y2)*(x2-x1))==((y2-y1)*(x-x2)))
-                        con++;
-                }
-                ans = Math.max(con,ans);
+                double a = y2-y1;
+                double b = x2-x1;
+                double angle = a/b;
+                map.put(angle,map.getOrDefault(angle,0)+1);
+                ans = Math.max(map.get(angle)+1,ans);
             }
         }
-        if(points.length==1)
-            return 1;
-        
         return ans;
     }
 }
