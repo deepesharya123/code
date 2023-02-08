fe51dc183275d6e9ef72901e95996f3c456ca974
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2023-02-08 14:05:51 +0530

    Time: 152 ms (13.81%), Space: 42.5 MB (89.30%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,20 +1,25 @@
 class Solution {
-    
+    int[] dp = new int[10005];
+    public int solve(int[] nums, int ind){
+        if(ind== nums.length-1)
+            return 0;
+        
+        if(dp[ind]!=-1)
+            return dp[ind];
+        
+        int re =(int) (1e7);
+        int jumps = nums[ind], k = ind+1;
+        while(jumps-->0 && k<nums.length){
+            int r = 1+ solve(nums,k);
+            k++;
+            re = Math.min(re,r);
+        }
+        dp[ind]=re;
+        return re;
+    }
     
     public int jump(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
-        Arrays.fill(res,Integer.MAX_VALUE);
-        res[0]=0;
-        for(int i = 0;i<nums.length;i++){
-            int j = nums[i], k = i+1;
-            while(j-->0 && k< nums.length){
-                res[k] = Math.min(res[k],res[i]+1);
-                k++;
-            }
-            
-        }
-        
-        return res[nums.length-1];
+        Arrays.fill(dp,-1);
+        return solve(nums,0);
     }
 }
