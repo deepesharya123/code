c497f931fe75a76c0f7959f70d79f213e5fb5aae
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2023-02-15 12:43:23 +0530

    Time: 5 ms (49.57%), Space: 43.7 MB (85.09%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -1,57 +1,18 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-       List<Integer> l = new ArrayList<>();
-        for(int a : num)
-            l.add(a);
+        List<Integer> l = new ArrayList<>();
+        int carry = 0, i = num.length-1;
         
-        int c = 0;
-        int i = l.size()-1;
-        while(i>=0 &&  k>0){
-            int rem = k%10;
-            int n =l.get(i); 
-            l.remove(i);
-            int add = rem+n+c;
-            if(add>9){
-                c = 1;
-                l.add(i--,add%10);
-            }else{
-                c=0;
-                l.add(i--,add%10);
-            }
+        while(k>0 || carry>0 || i>=0){
+            int n = i>=0 ? num[i--]:0;
+            int add = k%10;
+            add+=n+carry;
+            carry = add/10;
+            l.add(add%10);
             k/=10;
             
         }
-        while(i>=0 ){
-            int rem = 0;
-            int n =l.get(i); 
-            l.remove(i);
-            int add = rem+n+c;
-            if(add>9){
-                c = 1;
-                l.add(i--,add%10);
-            }else{
-                c=0;
-                l.add(i--,add%10);
-            }
-            
-            
-        }
-        while(k>0){
-            int rem = k%10;
-            k/=10;
-            int add = rem+c;
-            if(add>9){
-                c = 1;
-                l.add(0,add%10);
-            }else{
-                c=0;
-                l.add(0,add%10);
-            }
-            
-        }
-        if(c>0){
-            l.add(0,c);
-        }
+        Collections.reverse(l);
         return l;
     }
 }
