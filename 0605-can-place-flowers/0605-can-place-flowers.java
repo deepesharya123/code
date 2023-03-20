76176129dd62bccd0d475d662267dca1416f6d80
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2023-03-20 15:50:59 +0530

    Time: 1 ms (100.00%), Space: 44.4 MB (16.79%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,58 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if(flowerbed[0] == 0 && n==1 && flowerbed.length ==1) return true;
+        int count = 0, zero = 0;
+        boolean prevOne = false;
+        for(int num : flowerbed){
+            if(prevOne == false){
+                if(num == 0){
+                    zero++;
+                }else{
+                    count+=zero/2;
+                    zero = 0 ;
+                    prevOne = true;
+                }
+            }else{
+                if(num == 0){
+                    zero++;
+                }else{
+                    count+=(zero-1)/2;
+                    zero = 0;
+                }
+            }
+        }
+        if(prevOne)
+             count+=(zero)/2;
+        else
+            count+=(zero+1)/2;
+        return count >= n;
+    }
+}
+
+/*
+
+[1,0,0,0,1]
+1
+[1,0,0,0,1]
+2
+[1,0,0,0,1]
+3
+[1,0,0,0,0,1,0,1,0,0,0,0]
+1
+[1,0,0,0,0,1,0,1,0,0,0,0]
+2
+[1,0,0,0,0,1,0,1,0,0,0,0]
+3
+[1,0,0,0,0,1,0,1,0,0,0,0]
+4
+[1,0,0,0,0,1,0,1,0,0,0,0]
+5
+[1,0,0,0,0,1,0,1,0,0,0,0]
+6
+[1,0,0,0,0,1,0,1,0,0,0,0]
+7
+[1,0,0,0,0,1,0,1,0,0,0,0]
+8
+
+
+*/
