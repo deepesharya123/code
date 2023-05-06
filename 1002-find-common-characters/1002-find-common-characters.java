2fb6b87ddce479d65d933042ec479b947e584d79
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2023-05-06 16:00:48 +0530

    Time: 21 ms (20.14%), Space: 43.6 MB (7.42%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -1,33 +1,35 @@
 class Solution {
     public List<String> commonChars(String[] words) {
-        List<String> ans = new ArrayList<>();
-        for( int i = 0 ; i < 26 ; i++){
-            char c =(char) ((int)'a' +i);
-            int presentInAll = 0;
-            int minCount = Integer.MAX_VALUE;
-            for(String s: words){
-                int count = 0;
-                int presence = 0;
-                for(char cc : s.toCharArray()){
-                    if(cc == c){
-                        if(presence == 0)
-                            presence++;
-                        count++;
-                    }
-                }
-                presentInAll+=presence;
-                
-                minCount = Math.min(minCount, count);
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c = 'a';c<='z';c++)
+            map.put(c,Integer.MAX_VALUE);
+        for( String s: words){
+            HashMap<Character, Integer> temp = new HashMap<>();
+            for(char c = 'a';c<='z';c++)
+                temp.put(c,0);
+            for( char c : s.toCharArray()){
+                temp.put(c, temp.get(c)+1);
             }
-            
-            if(presentInAll == words.length){
-                int add = minCount != Integer.MAX_VALUE ? minCount : 0;
-                while(add > 0 ){
-                    ans.add(""+c);
-                    add--;
-                }   
+        
+            for(char c = 'a' ; c<= 'z' ;c++){
+                if(temp.containsKey(c))
+                    map.put(c, Math.min(map.get(c),temp.get(c)));
+            }
+        }
+        List<String> ans = new ArrayList<>();
+        for(char c = 'a' ; c<= 'z' ;c++){
+            int add = map.get(c);
+            while(add > 0 && add != Integer.MAX_VALUE){
+                ans.add(""+c);
+                add--;
             }
         }
         return ans;
     }
 }
+
+/*
+
+["bella","label","roller"]
+
+*/
