a617755a66d9dc1856d38c6313ebe2f8309b77a0
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2023-05-12 13:02:07 +0530

    Time: 6 ms (88.36%), Space: 95.4 MB (74.07%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,6 +1,5 @@
 class Solution {
     public long mostPoints(int[][] questions) {
-        long ans = 0;
         int n = questions.length;
         long[] dp = new long[n+2];
         dp[n] = dp[n+1] = 0;
@@ -10,11 +9,11 @@ class Solution {
             
             long inc = points, dis = 0;
             if(i+skip+1 < dp.length)
-            inc+=dp[i+skip+1];
+                inc+=dp[i+skip+1];
             dis+=dp[i+1];
             dp[i] = Math.max(inc,dis);
-            ans = Math.max(dp[i], ans);
         }
-        return ans; 
+       
+        return dp[0]; 
     }
 }
