a30feeda8b73dd2895d0f659f5e13e4278899aef
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2023-05-27 23:21:41 +0530

    Time: 2 ms (100.00%), Space: 43.5 MB (25.00%) - LeetHub

--- 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.java
+++ 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.java
@@ -1,43 +1,18 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList();
-    
-    public void solve(int[] nums, int ind, List<Integer> temp){
-        int n = nums.length;
-        if(ind>= n){
-            ans.add(new ArrayList(temp));
-            return;
-        }
-            
-//         discard current element
-        solve(nums, ind+1, temp);
-//          include current index
-        temp.add(nums[ind]);
-        solve(nums, ind+1, temp);
-        temp.remove(temp.indexOf(nums[ind]));
-    }
-    
     public long maxStrength(int[] nums) {
-        long res = Integer.MIN_VALUE;
-        solve(nums, 0, new ArrayList());
-        for(List<Integer> list : ans){
-            if(list.size() == 0)
-                continue;
-            long sum = list.get(0);
-            for(int i = 1 ; i < list.size(); i++)
-                sum*=list.get(i);
-            res = Math.max(res,sum);
+        long res = 1;
+        int largest = Integer.MIN_VALUE,neg = 0, largestNegative = Integer.MIN_VALUE;
+        for(int num :  nums){
+            if(num!=0)res*=num;
+            if(num < 0){
+                neg++;
+                largestNegative = Math.max(largestNegative, num);
+            }
+            largest = Math.max(largest, num);
         }
-        return res;
+        if(largest == 0 && neg < 2)  return 0;
+        if(largest < 0 && neg == 1 ) return (largestNegative);
+        if(res > 0) return res;
+        return res/largestNegative;
     }
 }
-
-/*
-
-[-4,-5]
-[3,-1,-5,2,5,-9]
-[3,-1,-5,2,5,-9,1,2,3,4,5,6,7]
-[0,-1]
-
-
-
-*/
