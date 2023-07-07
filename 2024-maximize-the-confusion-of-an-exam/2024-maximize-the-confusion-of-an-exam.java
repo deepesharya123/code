a0db768895095e39302265782ac35e289081f719
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2023-07-07 11:32:37 +0530

    Time: 15 ms (80.31%), Space: 43.7 MB (65.00%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -6,14 +6,18 @@ class Solution {
             char c = answerKey.charAt(j);
             if( c == 'T' ) tru++;
             else fals++;
-            
+//              true is occuring less than k times, so false is more than k times, we can change only trues
             if( tru <= k ){
                 ans = Math.max(ans, j-i + 1);
                 j++;
-            }else if( fals <= k){
+            }
+//              false is occuring less than k times, so true is more than k times, we can change only false
+            else if( fals <= k){
                 ans = Math.max(ans, j-i + 1);
                 j++;    
-            }else {
+            }
+//             if both true and false are occuring more than k times , then compress the size of window untill we have either true or false less k times so that we can change that
+            else {
                 while( tru > k && fals > k && i <= j){
                     char ch = answerKey.charAt(i++);
                     if( ch == 'T') tru--;
