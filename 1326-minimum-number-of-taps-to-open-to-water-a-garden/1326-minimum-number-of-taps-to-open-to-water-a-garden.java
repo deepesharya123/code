cf4c72a1e0e2df332d617e2fbe9377de5a6ed3be
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2023-08-31 17:13:47 +0530

    Time: 24 ms (5.26%), Space: 44.6 MB (14.37%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -4,11 +4,11 @@ class Solution {
     public long solve(int ind, int[][] intervals){
         int len = intervals.length;
         
-        if(intervals[ind][1] == len-1)
-            return 1;
-        
         if(ind >= len)
             return Integer.MAX_VALUE;
+        
+        if(intervals[ind][1] >= len-1)
+            return 1;
         
         if(dp[ind] != -1)
             return dp[ind];
@@ -28,15 +28,17 @@ class Solution {
         int[][] intervals = new int[n+1][2];
         int i = 0 ;
         for(i = 0 ; i <= n; i++){
-            intervals[i][0] = Math.max(0, i - ranges[i]);
-            intervals[i][1] = Math.min(n, i + ranges[i]);
+            // intervals[i][0] = Math.max(0, i - ranges[i]);
+            // intervals[i][1] = Math.min(n, i + ranges[i]);
+            intervals[i][0]  = i-ranges[i];
+            intervals[i][1] = i+ranges[i];
         }
         
         long ans = Integer.MAX_VALUE;
         Arrays.fill(dp,-1);
         Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
         for(i = 0 ; i <= n; i++){
-            if(intervals[i][0] == 0){
+            if(intervals[i][0] <= 0){
                 ans = Math.min(ans, solve(i, intervals));
             }
         }
