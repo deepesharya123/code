894d1154443f5c3101a7101100618697e602811b
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2023-11-16 17:28:57 +0530

    Time: 9 ms (24.00%), Space: 41.1 MB (29.45%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -0,0 +1,52 @@
+class Solution {
+    
+    public int binaryToDecimal(String s){
+        int ans = 0 ;
+        int i = 0 , n = s.length();
+        while( i < n ){
+            char c = s.charAt(i++);
+            ans*=2;
+            if( c == '1')
+                ans++;
+        }
+        return ans;
+    }
+    
+    public String findDifferentBinaryString(String[] nums) {
+        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>();
+        for(String s : nums){
+            set.add(binaryToDecimal(s));
+        }
+        String ans = "";
+        // System.out.println(set);
+        int i = 0;
+        while( true ){
+            if( ans.length() > 0 )
+                break;
+            if( !set.contains(i) ){
+                int num = i;
+                // System.out.println(num+" num i check "+i);
+                
+                if( i == 0 ){
+                    while( n > 0){
+                        ans+="0";
+                        n--;
+                    }
+                    break;
+                }
+                while( num > 0 ){
+                    int d = num % 2;
+                    ans = d +ans;
+                    num/=2;
+                }
+                break;
+            }
+            i++;
+        }
+        n = nums.length;
+        while( ans.length() < n)
+            ans = "0"+ans;
+        return ans;
+    }
+}
