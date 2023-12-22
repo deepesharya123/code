25770b1a815f323818eeb210e9165798f9359258
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2023-12-22 18:58:21 +0530

    Time: 37 ms (7.11%), Space: 43.2 MB (31.56%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxScore(String s) {
+        int ans = Integer.MIN_VALUE;
+        int n = s.length();
+        int totalOnes = 0, curOne = 0, zero = 0;
+        for(int i = 0 ; i < n ; i++ ){
+            char c = s.charAt(i);
+            totalOnes+= c =='1' ? 1 : 0;
+        }
+        
+        for(int i = 0 ; i < n-1 ; i++ ){
+            char c = s.charAt(i);
+            zero+= c == '0' ? 1 : 0;
+            curOne+= c =='1' ? 1 : 0;
+            System.out.println(zero+" "+curOne+" "+c);
+            int temp = zero + (totalOnes-curOne);
+            ans = Math.max(temp, ans);
+        }
+        
+       return ans;
+    
+    }
+}
+
+
+/*
+
+
+"011101"
+"00111"
+"1111"
+"111111"
+"000"
+"0000"
+"0100"
+
+
+
+*/
