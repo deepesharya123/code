2cced8061d9da278f36a62ebf3ae0b274d97950c
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2024-03-09 10:55:03 +0530

    Time: 7 ms (34.44%), Space: 62.6 MB (57.41%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,20 +1,44 @@
 class Solution {
+    
+    public int bs(int[]ar , int x){
+        int n = ar.length;
+        int i = 0 , j = n-1;
+        while( i <= j ){
+            int mid = i + (j-i)/2;
+            int num = ar[mid];
+            if( num == x ) 
+                return i;
+            
+            if( num > x )
+                j = mid - 1 ;
+            else
+                i = mid + 1;
+        }
+        return -1;
+    }
+    
     public int getCommon(int[] nums1, int[] nums2) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int num : nums1 )
-            set.add(num);
         int ans = Integer.MAX_VALUE;
-        for(int i = 0 ; i < nums2.length - 1 ; i++  ){
-            if( nums2[i] == nums2[i + 1] )
-                continue;
-            if( set.contains(nums2[i]) ){
-                ans = Math.min(ans, nums2[i]);
+        for(int num : nums1 ){
+            int found = bs(nums2, num );
+            if( found >= 0 ){
+                ans = num;
                 break;
             }
         }
-        if( set.contains(nums2[nums2.length - 1 ]) )
-            ans = Math.min(ans, nums2[nums2.length - 1 ]);
-
-        return ans == Integer.MAX_VALUE ? -1 : ans;
+        return ans == Integer.MAX_VALUE ? -1  : ans;
     }
 }
+
+/*
+
+[1,2,3]
+[2,4]
+[1,2,3,6]
+[2,3,4,5]
+[1,2,3,4,56]
+[6,7,8,8]
+[1,2]
+[2]
+
+*/
