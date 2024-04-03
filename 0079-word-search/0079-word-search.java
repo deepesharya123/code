c2f74b03e8b683e2a66b6f72e52df9e3d46ef03f
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2024-04-03 10:31:34 +0530

    Time: 157 ms (44.28%), Space: 44.9 MB (20.96%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,62 +1,53 @@
 class Solution {
     
-    public boolean solve(char[][] board, int[][] vis, String word, int ind, int r, int c){
-        int sl = word.length();
-        if( ind == sl )
+    public boolean solve(int cr , int cc , String word, char[][] board , int wi ,int[][] vis){
+        if( wi +1 >=  word.length() )
             return true;
+        
+        int[] d = {-1 , 0,1,0,-1};
+        
         int n = board.length, m = board[0].length;
-        
-        if( r < 0 || r >= n || c < 0 || c >= m || ( board[r][c] != word.charAt(ind) ) || (board[r][c] == word.charAt(ind) && vis[r][c] == 1) ) 
-            return false;
-        
-        
-        int[] dr = {-1,0,1,0};
-        int[] dc = {0,1,0,-1};
-        vis[r][c] = 1;
-        
-        for(int i = 0 ; i < 4; i++){
-            int nr = r+dr[i];
-            int nc = c+dc[i];
-            
-            if( solve(board, vis, word, ind+1, nr,nc)){
-                return true;
+        boolean ret = false;
+        for(int i = 0 ; i < 4 ;i++ ){
+            int nr = cr + d[i] , nc = cc + d[i+1];
+            if( nr >= 0 && nr < n && nc >= 0 && nc < m && board[nr][nc] == word.charAt(wi+1) && vis[nr][nc] == 0){
+                vis[nr][nc] = 1;
+                ret |= solve(nr, nc , word , board, wi+1,vis);
+                vis[nr][nc] = 0;
+                if( ret ){
+                    return ret;
+                }
             }
         }
         
-        vis[r][c] = 0;
         return false;
     }
     
     public boolean exist(char[][] board, String word) {
-        int n = board.length, m = board[0].length;
-        int[][] vis = new int[n][m];
-        boolean ret = false;
-        
-        for( int i = 0 ; i < n ;i++){
-            for( int j = 0; j < m ;j++){
-                if( word.charAt(0) == board[i][j] &&  solve(board, vis, word, 0,i,j)){
-                    return true;
+        int[][] vis = new int[board.length][board[0].length];
+        for(int i = 0 ; i < board.length ; i++ ){
+            for(int j = 0 ; j < board[0].length; j++ ){
+                if( board[i][j] == word.charAt(0) ){
+                    vis[i][j] = 1;
+                    boolean ans = solve( i , j , word, board ,0,vis);
+                    vis[i][j] = 0;
+                    if( ans )
+                        return ans;
                 }
             }
         }
-        System.out.println("final false");
+        
         return false;
     }
 }
 
-
 /*
-
-[["A"]]
-"A"
-[["A","B"]]
-"AB"
 [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
 "ABCCED"
 [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
 "SEE"
 [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
 "ABCB"
-
-
+[["C","A","A"],["A","A","A"],["B","C","D"]]
+"AAB"
 */
